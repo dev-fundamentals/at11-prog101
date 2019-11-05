edd9992a80b58347e8263fb593d23e88702e8828
Enrique <Enrique.Carrizales@fundacion-jala.org>
2019-11-05 12:13:31 -0400

    Update GuessNumber package

--- src/org/funda/prog101/enrique/udacityTraining/src/GuessNumber/Game.java
+++ src/org/funda/prog101/enrique/udacityTraining/src/GuessNumber/Game.java
@@ -8,28 +8,29 @@ public class Game {
 		int numberToGuess = (int) (Math.random() * 100) + 1;
 		int numberAnswer;
 		int oportunities = 10;
+		int lastChance = 9;
 		
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Wellcome to Guess The Number\nYou have 10 tries");
 		
 		for (int i = 0; i < oportunities; i++) {
-			int oportunitiesLeft = i - oportunities - 1;
+			int oportunitiesLeft =  oportunities - i -1;
 			System.out.println("Introduce your answer: ");
 			numberAnswer = Integer.parseInt(scanner.nextLine());
 			
 			if (numberAnswer == numberToGuess) {
 				System.out.println("You Win!\nThe answer is: " + numberAnswer);
-				i = 10;
+				i = oportunities;
 			}else if (numberAnswer > numberToGuess) {
 				System.out.println("The number is less than: " + numberAnswer + "\n");
-				System.out.println("You have: " + oportunitiesLeft + " times left" + "\n");
+				System.out.println("You have: " + oportunitiesLeft + " chance left" + "\n");
 			} else {
 				System.out.println("The number is more than: " + numberAnswer + "\n");
-				System.out.println("You have: " + oportunitiesLeft + " times left" + "\n");
+				System.out.println("You have: " + oportunitiesLeft + " chance left" + "\n");
 			}
 			
-			if (i == 9) {
+			if (i == lastChance) {
 				System.out.println("You lose!\nThe answer was: " + numberToGuess + "\n");
 			}
 		}
